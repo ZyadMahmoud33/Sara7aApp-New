3a3d32e2aff7019fcd3d0bd18218ab65e951d6ac
ZyadMahmoud33 <zyad.mahmoud33@gmail.com>
2026-05-11 11:18:16 +0300

    fix: remove file logging to fix Vercel EROFS error

--- src/Utlis/loggers/morgan.logger.js
+++ src/Utlis/loggers/morgan.logger.js
@@ -4,12 +4,18 @@ import morgan from "morgan";
 
 const __direname = path.resolve();
 
-export function attachRouterWithLogger(app, routerPath, router, logFileName) {
-    const logStream = fs.createWriteStream(
-        path.join(__direname, "./src/logger", logFileName),
-        { flags: "a" },
-    );
+// export function attachRouterWithLogger(app, routerPath, router, logFileName) {
+//     const logStream = fs.createWriteStream(
+//         path.join(__direname, "./src/logger", logFileName),
+//         { flags: "a" },
+//     );
 
-    app.use(routerPath, morgan("combined", { stream: logStream }), router);
+//     app.use(routerPath, morgan("combined", { stream: logStream }), router);
+//     app.use(routerPath, morgan("dev"), router);
+export function attachRouterWithLogger(app, routerPath, router, logFileName) {
+    // لغينا الـ logStream خالص لأنه بيسبب error: EROFS على فيرسيل
+    
+    // هنخلي morgan يطبع في الـ console بس
     app.use(routerPath, morgan("dev"), router);
 };
+
