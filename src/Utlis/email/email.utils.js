79e62a7f8fb6097c91a5ad590b02c8bf7deffbdf
ZyadMahmoud33 <zyad.mahmoud33@gmail.com>
2026-05-16 14:34:20 +0300

    fix: use Resend email with onboarding@resend.dev

--- src/Utlis/email/email.utils.js
+++ src/Utlis/email/email.utils.js
@@ -46,7 +46,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function sendEmail({ to = "", subject = "", text = "", html = "", cc = "", bcc = "", attachments = [] }) {
     try {
         const { data, error } = await resend.emails.send({
-            from: 'Sara7a <sniper2killyt@gmail.com.dev>', // الإيميل الافتراضي من Resend
+            from: 'Sara7a <onboarding@resend.dev>', // الإيميل الافتراضي من Resend
             to: [to],
             subject: subject,
             html: html,
